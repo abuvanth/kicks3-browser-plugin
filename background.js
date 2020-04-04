2c2d619cfc5ca4b0dc75ceaf592eabcc04246f31
Syed <20999104+abuvanth@users.noreply.github.com>
2020-04-04 15:14:30 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,24 @@
 // background.js
-
-// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
+});
+
+var info;
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    console.log("Sadsdsads")
+  // When we get a message from the content script
+  if(message.method == 'setInfo'){
+      console.log("setinfo")
+    info = message.info;
+  }
+    
+  // When we get a message from the popup
+  else if(message.method == 'getInfo'){
+      console.log("getinfo")
+    sendResponse(info);
+  }
 });
